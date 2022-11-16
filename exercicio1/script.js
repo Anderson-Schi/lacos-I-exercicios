bc89f9619da89419129673ccfa423534d6ced504
Anderson-Schi <jr93@hotmai.com>
2022-11-16 21:43:12 +0000

    exercicio 1 revisado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,9 +1,9 @@
-let pergunta = prompt("Voce deseja comer mais uma coxinha ?").toLocaleUpperCase();
+let pergunta = prompt("Voce deseja comer mais uma coxinha ?").toUpperCase();
 let conta = 0;
 
 
 while(pergunta === "S"){
-    pergunta = prompt("Voce deseja comer mais uma coxinha ?").toLocaleUpperCase();
+    pergunta = prompt("Voce deseja comer mais uma coxinha ?").toUpperCase();
     conta+=2.50;
 };
 console.log(`Saldo total R$${conta}`);
