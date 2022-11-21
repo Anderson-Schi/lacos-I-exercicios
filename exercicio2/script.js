3735b2637110853cbe6b7388e730401d630a22cf
Anderson-Schi <jr93@hotmai.com>
2022-11-21 19:23:18 +0000

    exercicios resolvidos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -2,4 +2,4 @@ let input = Number(prompt("Digite um numero "));
 
 for(let num = 0; num <= 10; num++ ){
     console.log(`${input}x${num} = ${input*num}`);
-}
+};
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -4,4 +4,4 @@ let rank = 0
 for(let i = 0; i < maioresPaises.length; i++){
     rank++
     console.log(`${rank} -`,maioresPaises[i])
-}
+};
